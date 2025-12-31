9be8b6b8724e491cb32ecd5b8e6ea9dbb4bb0d82
이태희 <47910104+takgukim@users.noreply.github.com>
2025-12-31 23:02:52 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# thWebViewAppAndroid
+웹뷰로 개발하는 안드로이드 앱 프로젝트
---
+++ app/src/main/java/com/example/thwebviewappandroid/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.thwebviewappandroid;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.window.OnBackInvokedDispatcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        web = findViewById(R.id.webview);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.setWebViewClient(new WebViewClient());
+        web.loadUrl("http://10.0.2.2:8080/index.html");
+
+        // 최신 안드로이드 방식
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+            getOnBackInvokedDispatcher().registerOnBackInvokedCallback(
+                    OnBackInvokedDispatcher.PRIORITY_DEFAULT,
+                    () -> {
+                        if (web.canGoBack()) {
+                            web.goBack();
+                        } else {
+                            finish();
+                        }
+                    });
+        }
+    }
+}
